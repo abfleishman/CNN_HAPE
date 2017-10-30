a569aa7ec6a662056129469e793162ce480f2c78
Abram Fleishman <abram@conservationmetrics.com>
2017-10-30 13:57:29 -0700

    matlab script

--- Untitled2.m
+++ Untitled2.m
@@ -1,4 +1,8 @@
 
+% hape=submitJob('fig2csvBatch',{'/mnt/NAS3_2Mar15/All_HAPE/HAPE2csv.txt','/mnt/NAS3_2Mar15/All_HAPE','','site_yyyymmdd_HHMMSS'},0);
+
+% job=submitJob( 'collectAuditInfo',{'/mnt','/mnt/NAS1_2Jun14/figDb.csv','fig'},0);
+
 hape = readCsvColumns('D:\CM,Inc\CNN_HAPE\hape_streams.csv',true,'audit_file','nneg','npos','outpath', 'outdir');
 path = cellfun(@(x) x(1), hape);
 nneg = cellfun(@(x) x(2), hape);
@@ -6,20 +10,19 @@ npos = cellfun(@(x) x(3), hape);
 outpath = cellfun(@(x) x(4), hape);
 outdir = cellfun(@(x) x(5), hape);
 
-failedpaths = zeros(808,1);
-[~,~]=mkdir(outdir);
+failedpaths =   num2str(zeros(length(npos),1));
 
-for i = 1:length(hape)
+for i = 51:length(hape)
     [~,~]=mkdir(char(outdir(i)));
-    warning([num2str(i),': ' ,char(path(1))])
+    warning([num2str(i),': ' ,char(path(i))])
     if str2num(char(npos(i))) == 0 & str2num(char(nneg(i))) > 0
-        auditorSplit(char(path(i)),  {char(outpath(i))},  '', [str2num(char(nneg(i)))], 'uniform');
+        auditorSplit(strrep(char(path(i)),'/','\'),  {char(outpath(i))},  '', [str2num(char(nneg(i)))], 'uniform');
     else
         if str2num(char(npos(i))) > 0 & str2num(char(nneg(i))) > 0
-           auditorSplit(char(path(i)),  {char(outpath(i))},  '', [str2num(char(nneg(i)))],'5', [str2num(char(npos(i)))],'uniform');
+           auditorSplit(strrep(char(path(i)),'/','\'),  {char(outpath(i))},  '', [str2num(char(nneg(i)))],'5', [str2num(char(npos(i)))],'uniform');
         else
             if str2num(char(npos(i))) > 0 & str2num(char(nneg(i))) == 0
-               auditorSplit(char(path(i)),  {char(outpath(i))},'5', [str2num(char(npos(i)))],'uniform');
+               auditorSplit(strrep(char(path(i)),'/','\'),  {char(outpath(i))},'5', [str2num(char(npos(i)))],'uniform');
             else
                 warning(['Failed: ' i path(i)])
                 failedpaths(i) = path(i)
@@ -30,9 +33,11 @@ end
 
 
             
-   
+   num2str
 
-auditorMerge({char(outpath(1)), char(outpath(2)), char(outpath(3))},'\\NAS1\NAS1_2Jun14\Motherships\HAPE_CNN_Mother_test\smoosh.fig')
+auditorMerge({'\\NAS1\NAS1_2Jun14\Motherships\HAPE_CNN_Mother_test\ABC_BCPE_2014/CM25_SD25_2014/CM25_SD25_2014_audit_dnnClass_HAPE_18Jun14.fig',...
+    '\\NAS1\NAS1_2Jun14\Motherships\HAPE_CNN_Mother_test\FIWI_2015_R2/LEHU1_2September15/LEHU1_2September15_audit_HAPE_21Apr.fig'},...
+    '\\NAS1\NAS1_2Jun14\Motherships\HAPE_CNN_Mother_test\smoosh.fig')
 
 auditor('\\NAS1\NAS1_2Jun14\Motherships\HAPE_CNN_Mother_test\KESRP_HNKP_2015_R2\HNKP3_28October15','HNKP3_28October15_audit_HAPE_21Apr')
 auditorSaveProject('\\NAS1\NAS1_2Jun14\Motherships\HAPE_CNN_Mother_test\KESRP_HNKP_2015_R2\HNKP3_28October15','HNKP3_28October15_audit_HAPE_21Apr')
